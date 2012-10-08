71570db844f909327f22aa19ea10d281d42aeae9
etsai <thegod0fde4th@gmail.com>
2012-10-08 02:02:35 -0700

    Added command line options to set udp port and database path

--- main.cpp
+++ main.cpp
@@ -1,36 +1,67 @@
 #include <iostream>
+#include <string>
 #include <sqlite3.h>
 
+#include "Stoughton1004Lib/CLI/cli.h"
+#include "Stoughton1004Lib/Logging/logging.h"
+#include "Stoughton1004Lib/Network/DatagramPacket.h"
+#include "Stoughton1004Lib/Network/DatagramSocket.h"
+
+using namespace Stoughton1004Lib;
 using std::cerr;
 using std::cout;
 using std::endl;
+using std::string;
 
-static int callback(void *NotUsed, int argc, char **argv, char **azColName){
-    int i;
-    for(i=0; i<argc; i++){
-      printf("%s = %s\n", azColName[i], argv[i] ? argv[i] : "NULL");
-    }
-    printf("\n");
-    return 0;
-}
+Logger *logger;
+ConsoleHandler consoleH;
+FileHandler fileH;
 
 int main(int argc, char **argv) {
+    string dbPath;
+    int udpPort;
+    CLIBuilder *cli= CLIBuilder::getBuilder();
+
+    cli->setUsage("kfsxtslite [options]");
+    cli->addOption(Option("-port", 0, 1, [&udpPort](const Arguments &args) -> void {
+        udpPort= args.asInteger(0);
+    }).withDescription("UDP port to listen for packets").withArgName("no"))
+    .addOption(Option("-dbpath", 0, 1, [&dbPath](const Arguments &args) -> void {
+        dbPath= args.asString(0);
+    }).withDescription("Sqlite database to use").withArgName("path").withRequired(true))
+    .addOption(Option("-h", [&cli](const Arguments &args) -> void {
+        cli->displayUsage();
+    }));
+    cli->parse(argc, argv);
+
+    logger= Logger::getLogger("KFSXTrackingServerLite");
+
     sqlite3 *db;
     int rc;
-    char *zErrMsg = 0;
+    char *zErrMsg;
 
-    rc= sqlite3_open("kfsxdb.sqlite", &db);
+    logger->addHandler(&consoleH);
+    logger->addHandler(&fileH);
+    rc= sqlite3_open(dbPath.c_str(), &db);
+
     if (rc) {
-        cout << "Error opening db" << endl;
+        logger->log(Level::SEVERE, "Cannot open database: " + dbPath);
         return -1;
     }
 
-    rc = sqlite3_exec(db, "select * from difficulties", callback, 0, &zErrMsg);
-    if (rc!=SQLITE_OK){
-        cerr << "SQL error: " << zErrMsg << endl;
+    auto callback= [](void *NotUsed, int argc, char **argv, char **azColName) -> int {
+        int i;
+        for(i=0; i<argc; i++){
+            logger->log(Level::INFO, string(azColName[i]) + " = " + (argv[i] ? argv[i] : "NULL"));
+        }
+        return 0;
+    };
+
+    rc= sqlite3_exec(db, "select * from difficulties", callback, NULL, &zErrMsg);
+    if (rc!=SQLITE_OK) {
+        logger->log(Level::SEVERE, string("SQL error: ") + zErrMsg);
         sqlite3_free(zErrMsg);
     }
-
     sqlite3_close(db);
     return 0;
 }
