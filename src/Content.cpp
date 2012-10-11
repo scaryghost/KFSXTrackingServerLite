98f30415c0e2f62d7fbee21a31627019ca664b09
etsai <thegod0fde4th@gmail.com>
2012-10-11 02:57:31 -0700

    Fixed more compile time errors

--- src/Content.cpp
+++ src/Content.cpp
@@ -25,7 +25,7 @@ Content::~Content() {
 }
 
 Content& Content::updateDiff(const std::string &name, const std::string &length, int wins, int losses, int wave, Time time) {
-    char errMsg[128];
+    char *errMsg;
     stringstream select(stringstream::out), upsert(stringstream::out);
     int id= Utils::hashCode(name + "-" + length);
     auto diffTable= [&wins, &losses, &wave, &time](void *tableName, int argc, char **argv, char **colName) -> int {
@@ -43,13 +43,13 @@ Content& Content::updateDiff(const std::string &name, const std::string &length,
         id << ", coalesce(( select name from difficulties where id=" << id << "),\'" << name << "\'), " << 
         "coalesce(( select length from difficulties where id=" << id << "),\'" << length << "\'), " <<
         wins << ", " << losses << ", " << wave << ", \'" << time.toString() << "\');";
-    sqlite3_exec(db, upsert.str().c_str(), NULL, NULL, errMsg);
+    sqlite3_exec(db, upsert.str().c_str(), NULL, NULL, &errMsg);
 
     return *this;
 }
 
 Content& Content::updateLevel(const std::string &name, int wins, int losses, Time time) {
-    char errMsg[128];
+    char *errMsg;
     stringstream select(stringstream::out), upsert(stringstream::out);
     int  id= Utils::hashCode(name);
     auto levelTable= [&wins, &losses, &time](void *tableName, int argc, char **argv, char **colName) -> int {
@@ -65,13 +65,13 @@ Content& Content::updateLevel(const std::string &name, int wins, int losses, Tim
     upsert << "replace into levels (id, name, wins, losses, time) values (" <<
         id << ", coalesce(( select name from levels where id=" << id << "),\'" << name << "\'), " <<
         wins << ", " << losses << ", \'" << time.toString() << "\');";
-    sqlite3_exec(db, upsert.str().c_str(), NULL, NULL, errMsg);
+    sqlite3_exec(db, upsert.str().c_str(), NULL, NULL, &errMsg);
 
     return *this;
 }
 
 Content& Content::updatePlayer(const std::string &steamID64, const std::string &category, std::unordered_map<std::string, int> stats) {
-    char errMsg[128];
+    char *errMsg;
     stringstream select(stringstream::out), upsert(stringstream::out);
     int id= Utils::hashCode(steamID64 + "-" + category);
     vector<string> newStats(stats.size());
@@ -81,9 +81,9 @@ Content& Content::updatePlayer(const std::string &steamID64, const std::string &
         for(auto it= statPairs.begin(); it != statPairs.end(); it++) {
             vector<string> keyval= Utils::split(*it, '=');
             if (stats.count(keyval[0]) == 0) {
-                stats[keyval[0]]= atoi(keyval[1]);
+                stats[keyval[0]]= atoi(keyval[1].c_str());
             } else {
-                stats[keyval[0]]+= atoi(keyval[1]);
+                stats[keyval[0]]+= atoi(keyval[1].c_str());
             }
         }
         return 0;
@@ -99,14 +99,14 @@ Content& Content::updatePlayer(const std::string &steamID64, const std::string &
     }
     upsert << "replace into player (id, steamid, stats, category) values (" << 
         id << ", coalesce(( select steamid from player where id=" << id << "),\'" << steamID64 << "\'), \'" << 
-        Utils::join(newStats) << "\', coalesce(( select category from player where id=" << id << "),\'" << category << "\'));";
-    sqlite3_exec(db, upsert.str().c_str(), NULL, NULL, errMsg);
+        Utils::join(newStats, ',') << "\', coalesce(( select category from player where id=" << id << "),\'" << category << "\'));";
+    sqlite3_exec(db, upsert.str().c_str(), NULL, NULL, &errMsg);
 
     return *this;
 }
 
 Content& Content::updateAggregate(const std::string &category, const std::string &stat, long value) {
-    char errMsg[128];
+    char *errMsg;
     stringstream select(stringstream::out), upsert(stringstream::out);
     int id= Utils::hashCode(stat + "-" + category);
     auto aggregateTable= [&value](void *tableName, int argc, char **argv, char **colName) -> int {
@@ -120,13 +120,13 @@ Content& Content::updateAggregate(const std::string &category, const std::string
     upsert << "replace into aggregate (id, stat, value, category) values (" << 
         id << ", coalesce(( select stat from aggregate where id=" << id << "),\'" << stat << "\'), \'" << value << 
         "\', coalesce(( select category from aggregate where id=" << id << "),\'" << category << "\'));";
-    sqlite3_exec(db, upsert.str().c_str(), NULL, NULL, errMsg);
+    sqlite3_exec(db, upsert.str().c_str(), NULL, NULL, &errMsg);
 
     return *this;
 }
 
 Content& Content::updateRecord(const std::string &steamID64, int wins, int losses, int disconnects) {
-    char errMsg[128];
+    char *errMsg;
     stringstream select(stringstream::out), upsert(stringstream::out);
     int id= Utils::hashCode(steamID64);
     auto recordTable= [&wins, &losses, &disconnects](void *tableName, int argc, char **argv, char **colName) -> int {
@@ -142,13 +142,13 @@ Content& Content::updateRecord(const std::string &steamID64, int wins, int losse
     upsert << "replace into records (id, steamid, wins, losses, disconnects) values (" << 
         id << ", coalesce(( select steamid from records where id=" << id << "),\'" << steamID64 << "\'), " << 
         wins << ", " << losses << ", " << disconnects << ");";
-    sqlite3_exec(db, upsert.str().c_str(), NULL, NULL, errMsg);
+    sqlite3_exec(db, upsert.str().c_str(), NULL, NULL, &errMsg);
 
     return *this;
 }
 
 Content& Content::updateDeaths(const std::string &name, int value) {
-    char errMsg[128];
+    char *errMsg;
     stringstream select(stringstream::out), upsert(stringstream::out);
     int id= Utils::hashCode(name);
     auto deathTable= [&value](void *tableName, int argc, char **argv, char **colName) -> int {
@@ -161,7 +161,7 @@ Content& Content::updateDeaths(const std::string &name, int value) {
 
     upsert << "replace into deaths (id, name, count) values (" << id << 
         ", coalesce(( select name from deaths where id=" << id << "),\'" << name << "\'), " << value << ");";
-    sqlite3_exec(db, upsert.str().c_str(), NULL, NULL, errMsg);
+    sqlite3_exec(db, upsert.str().c_str(), NULL, NULL, &errMsg);
     
     return *this;
 }
